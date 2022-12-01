9e4977742b066f01a86695072ae753a75fc95bbb
Akingz2000 <seyiakintan@yahoo.com>
2022-12-01 11:48:09 -0800

    Electric Cars

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const people = [
+    {
+
+      image:
+        "./web-car/hyundai-ioniq-6-2022-001-min.png",
+      name: 'Hyundai Ioniq',
+      cost: '$20,000',
+      comapny: 'Hyundai plc',
+      aval: "yes",
+      func:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+    },
+
+    {
+
+      image:
+      "./web-car/75-nissan-leaf-review.jpg",
+      name: 'Nissan Leaf',
+      cost: '$18,000',
+      comapny: 'Nissan plc',
+      aval: "yes",
+      func:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+    },
+
+    {
+
+      image:
+      "./web-car/2019-chevrolet-volt_100666276_h.jpg",
+      name: 'chevrolet volt',
+      cost: '$15,000',
+      comapny: 'benz comp plc',
+      aval: "yes",
+      func:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+    },
+
+    {
+
+        image:
+        "./web-car/FordFocus.jpg",
+        name: 'Ford focus',
+        cost: '$27,000',
+        comapny: 'ford plc',
+        aval: "yes",
+        func:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+      },
+
+
+  ];
+
+  export default people;
---
+++ src/data2.js
@@ -0,0 +1,46 @@
+const people3 = [
+    {
+
+        image:
+          "./web-car/Chevrolet_Volt_WAS_2017_1739.jpg",
+        name: '2017 Chevrolet Volt',
+        cost: '$20,000',
+        comapny: 'Chevrolet plc',
+        availability: "no",
+        functionality:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+      },
+      {
+        id: 2,
+        image:
+        "./web-car/2022-Hyundai-Ioniq-Hybrid-382-1.webp",
+        name: '2022 Hyundai Ioniq',
+        cost: '$18,000',
+        comapny: 'Hyundai plc',
+        availability: "no",
+        functionality:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+      },
+      {
+        id: 3,
+        image:
+        "./web-car/2022-ford-focus-st.jpg",
+        name: '2022 Ford Focus',
+        cost: '$15,000',
+        comapny: 'Ford plc',
+        availability: "no",
+        functionality:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+      },
+      {
+          id: 4,
+          image:
+          "./web-car/2018-nissan-leaf-test-review-car-and-driver-photo-702887-s-original.jpg",
+          name: '2018 Nissan Leaf',
+          cost: '$27,000',
+          comapny: 'Nissan plc',
+          availability: "no",
+          functionality:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa molestiae error doloribus esse illum perferendis repellendus natus'
+        },
+
+
+]
+
+export default people3;
